8513262711c64188abe2e3530783c77f8fe8ecb1
Salumao Barbosa da Costa <salubcosta@gmail.com>
2026-06-16 22:49:21 -0300

    Component Header refatorado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,12 @@
-import { Link, useLocation } from 'react-router-dom';
+import Navigation from './Navigation';
 import './Header.css';
 
 const Header = () => {
 
-    const location = useLocation();
-
-    const links = [
-        { to: '/', label: 'Início' },
-        { to: '/categorias', label: 'Categorias' },
-        { to: '/projetos', label: 'Projetos' },
-        { to: '/atividades', label: 'Atividades' },
-    ]
-
     return (
         <header className="header">
             <span className="header-titulo">Atividades por <strong>Projetos</strong></span>
-            <nav>
-                {links.map(link => (
-                    <Link
-                        key={link.to}
-                        to={link.to} 
-                        className={location.pathname === link.to ? 'nav-link ativo': 'nav-link'}
-                    >
-                        {link.label}
-                    </Link>
-                ))}
-            </nav>
+            <Navigation />
         </header>
     );
 }
